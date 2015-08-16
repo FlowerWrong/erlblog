68d519701cc4453b4acf3fa719a569f53bff3767
yang <sysuyangkang@gmail.com>
2015-08-16 13:13:15 +0800

    Change to mysql and add db desgin

---
+++ scripts/erlblog.sql
@@ -0,0 +1,54 @@
+# Database design of erlblog.
+CREATE DATABASE IF NOT EXISTS `erlblog` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `erlblog`;
+
+SET FOREIGN_KEY_CHECKS=0;
+
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS posts;
+DROP TABLE IF EXISTS tags;
+DROP TABLE IF EXISTS post_tags;
+
+SET FOREIGN_KEY_CHECKS=1;
+
+
+CREATE TABLE users (
+  id MEDIUMINT NOT NULL AUTO_INCREMENT,
+  username VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE posts (
+  id MEDIUMINT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(255) NOT NULL,
+  summary TEXT NOT NULL,
+  content TEXT NOT NULL,
+  markdown TEXT DEFAULT NULL,
+  user_id MEDIUMINT NOT NULL,
+  PRIMARY KEY (id),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `post_user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE tags (
+  id MEDIUMINT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(255) NOT NULL,
+  PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE post_tags (
+  id MEDIUMINT NOT NULL AUTO_INCREMENT,
+  post_id MEDIUMINT NOT NULL,
+  tag_id MEDIUMINT NOT NULL,
+  PRIMARY KEY (id),
+  KEY `post_id` (`post_id`),
+  CONSTRAINT `tag_post_id` FOREIGN KEY (`post_id`) REFERENCES `posts` (`id`),
+  KEY `tag_id` (`tag_id`),
+  CONSTRAINT `post_tag_id` FOREIGN KEY (`tag_id`) REFERENCES `tags` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+# SHOW ENGINE INNODB STATUS;
