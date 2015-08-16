0a8714d7466a3aacd9e50d94d0dd02404b682d65
yang <sysuyangkang@gmail.com>
2015-08-16 15:02:44 +0800

    Add .keep

--- src/test/functional/erlblog_test.erl
+++ src/test/functional/erlblog_test.erl
@@ -2,7 +2,7 @@
 -compile(export_all).
 
 start() ->
-  boss_web_test:get_request("/welcome/hello", [],
+  boss_web_test:get_request("/posts", [],
     [ fun boss_assert:http_ok/1,
       fun(Res) -> boss_assert:tag_with_text("strong",
         "Rahm says hello!", Res) end ], []).
